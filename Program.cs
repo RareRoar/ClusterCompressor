0e8b6269db864a71fb3ba1222cb0d157766ad705
RareRoar <sirdysprozium@gmail.com>
2022-05-22 19:29:33 +0300

    Last method

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace ClusterCompressor
 
 
 
-
+            
             Form form = new Form();
             form.Text = "Image Viewer";
             form.Width = (int)(bitmap.Width * 1.2);
@@ -36,6 +36,8 @@ namespace ClusterCompressor
             int dh = 10;
             int wCount = width / dw + (width % dw > 0 ? 1 : 0);
             int hCount = height / dh + (height % dh > 0 ? 1 : 0);
+            
+
             //for (int di = 0; di < hCount; di++)
             //{
             //    for (int dj = 0; dj < wCount; dj++)
@@ -115,22 +117,62 @@ namespace ClusterCompressor
             //    }
             //}
 
+            //byte[] data = new byte[] { 0x01, 0x01, 0x01, 0x01, 0x02, 0x01, 0x01, 0x02 };//
             byte[] data = GaussianBlurer.BitmapToByteArray(bitmap);
             byte[] compressed = AltCompress(data);//RLEncoder.compress(data); //BWCompression.Compress(data);
-            byte[] decompressed = AltDecompress(compressed);//RLEncoder.decompress(compressed);//BWCompression.Decompress(compressed);
-            bitmap = GaussianBlurer.ByteArrayToBitmap(decompressed, width, height);
-            Console.WriteLine(data.Length);
-            Console.WriteLine(decompressed.Length);
+                                                  //byte[] decompressed = AltDecompress(compressed);//RLEncoder.decompress(compressed);//BWCompression.Decompress(compressed);
+                                                  //bitmap = GaussianBlurer.ByteArrayToBitmap(decompressed, width, height);
+            //for (int i = 0; i < data.Length; i++)
+            //{
+            //    Console.Write(Convert.ToString(data[i], 2).PadLeft(8, '0'));
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine();
+            //for (int i =0; i < compressed.Length; i++)
+            //{
+            //    Console.Write(Convert.ToString(compressed[i], 2).PadLeft(8, '0'));
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine();
+            byte[] decompressed = AltDecompress(compressed);
+            //for (int i = 0; i < decompressed.Length; i++)
+            //{
+            //    Console.Write(Convert.ToString(decompressed[i], 2).PadLeft(8, '0'));
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine(decompressed.Length);
 
             //pictureBox.Image = GaussianBlurer.Convolve(bitmap, GaussianBlurer.ApplyGaussianBlur(20, 20));
             //SwiftBlur sb = new SwiftBlur(bitmap);
             //pictureBox.Image = sb.Process(1);
+            bitmap = GaussianBlurer.ByteArrayToBitmap(decompressed, width, height);
+            Console.WriteLine(data.Length);
+            Console.WriteLine(compressed.Length);
+            Console.WriteLine(decompressed.Length);
 
+            for (int i = 0; i < data.Length; i++)
+            {
+
+                //Console.Write(i);
+                //Console.Write(") ");
+                //Console.Write(Convert.ToString(data[i], 2).PadLeft(8, '0'));
+                //Console.Write(" ");
+                //Console.Write(Convert.ToString(compressed[i], 2).PadLeft(8, '0'));
+                //Console.Write(" ");
+                //Console.WriteLine(Convert.ToString(decompressed[i], 2).PadLeft(8, '0'));
+                if (data[i] != decompressed[i])
+                {
+                    Console.Write(i);
+                    break;
+
+                }
+            }
             pictureBox.Image = bitmap;
             pictureBox.Dock = DockStyle.Fill;
             form.Controls.Add(pictureBox);
             pictureBox.Location = new Point(0, 0);
             Application.Run(form);
+            //Console.ReadKey();
         }
 
 
@@ -146,7 +188,11 @@ namespace ClusterCompressor
             {
                 if (dict[(data[i-2] << 8) + data[i - 1]] == data[i])
                 {
-                    result[currentByte] = (byte)(result[currentByte] | (0x01 << (8 - offset)));
+                    //Console.WriteLine("Guessed");
+                    //Console.WriteLine(currentByte);
+                    //Console.WriteLine(string.Format($"Byte: {0}, offset: {1}", currentByte.ToString(), offset));
+                    //Console.Write(Convert.ToString((0x01 << (8 - offset - 1)), 2).PadLeft(8, '0'));
+                    result[currentByte] = (byte)(result[currentByte] | (0x01 << (8 - offset - 1)));
                     offset++;
                     if (offset == 8)
                     {
@@ -164,9 +210,9 @@ namespace ClusterCompressor
                         currentByte++;
                     }
                     dict[(data[i - 2] << 8) + data[i - 1]] = data[i];
-                    result[currentByte] = (byte)(result[currentByte] & (data[i] >> offset));
+                    result[currentByte] = (byte)(result[currentByte] | (data[i] >> offset));
                     currentByte++;
-                    result[currentByte] = (byte)(result[currentByte] & (data[i] << (8 - offset)));
+                    result[currentByte] = (byte)(result[currentByte] | (data[i] << (8 - offset)));
                 }
             }
             byte[] temp = new byte[currentByte + 1];
@@ -186,16 +232,22 @@ namespace ClusterCompressor
             result.Add(data[0]);
             result.Add(data[1]);
             int currentByte = 2;
+            //Console.WriteLine(result.Count());
             while (currentByte < data.Length)
             {
-                int codeBit = (data[currentByte] >> (7 - offset)) % 2;
+                int codeBit = (data[currentByte] >> (8 - offset - 1)) % 2;
+                //Console.Write("Check bit: ");
+                //Console.WriteLine(codeBit);
                 if (codeBit == 1)
                 {
+                    //Console.Write("Written ");
                     int t = result[result.Count - 2];
                     t <<= 8;
                     t += result[result.Count - 1];
                     result.Add(dict[t]);
-                   // Console.WriteLine(result.Count.ToString() + ") 1added, cuurent" + currentByte.ToString());
+                    
+                    //Console.WriteLine(Convert.ToString((byte)(dict[t]), 2).PadLeft(8, '0'));
+                    // Console.WriteLine(result.Count.ToString() + ") 1added, cuurent" + currentByte.ToString());
                     offset++;
                     if (offset == 8)
                     {
@@ -203,54 +255,91 @@ namespace ClusterCompressor
                         currentByte++;
                         //Console.WriteLine("boom");
                     }
+                    if (currentByte == data.Length - 1)
+                    {
+                        continue;
+                    }
                 }
                 else
                 {
+                    if (currentByte == data.Length - 1)
+                    {
+                        break;
+                    }
                     offset++;
                     if (offset == 8)
                     {
                         offset = 0;
                         currentByte++;
-                       // Console.WriteLine("boom");
+                        //Console.WriteLine("boom");
                     }
 
                     int leftMask = 0b1;
-                    if (8-offset == 0)
+                    if (offset == 8)//todo
                     {
                         leftMask = 0;
                     }
                     else
                     {
-                        for (int j = 0; j < 8-offset; j++)
+                        for (int j = 0; j < 8-offset-1; j++)
                         {
                             leftMask <<= 1;
                             leftMask++;
                         }
                     }
                     int rightMask = 0b1;
-                    for (int j = 0; j < offset; j++)
-                    {
-                        rightMask <<= 1;
-                        rightMask++;
-                    }
-                    rightMask <<= (8 - offset);
+                    
+                        for (int j = 0; j < offset - 1; j++)
+                        {
+                            rightMask <<= 1;
+                            rightMask++;
+                        }
+                        rightMask <<= (8 - offset);
+                    
+
+                    
+                    //Console.WriteLine(currentByte);
+                    //Console.WriteLine(offset);
                     if (currentByte < data.Length - 1)
                     {
-                        result.Add((byte)(((data[currentByte] & leftMask) << 8) + (data[currentByte + 1] & rightMask)));
-                        int t = result[result.Count - 2];
+                        ////Console.WriteLine("Masks");
+                        ////Console.Write(Convert.ToString(leftMask, 2).PadLeft(8, '0'));
+                        ////Console.Write(" ");
+                        ////Console.WriteLine(Convert.ToString(rightMask, 2).PadLeft(8, '0'));
+
+                        ////Console.WriteLine("Data");
+                        ////Console.Write(Convert.ToString((byte)((data[currentByte])), 2).PadLeft(8, '0'));
+                        ////Console.Write(" ");
+                        ////Console.WriteLine(Convert.ToString((byte)(data[currentByte + 1]), 2).PadLeft(8, '0'));
+
+                        ////Console.WriteLine("Result");
+                        ////Console.Write(Convert.ToString((byte)((data[currentByte] & leftMask) << offset), 2).PadLeft(8, '0'));
+                        ////Console.Write(" ");
+
+                        //Console.Write("Written too ");
+                        result.Add((byte)(((data[currentByte] & leftMask) << offset) + ((data[currentByte + 1] & rightMask) >> (8 - offset))));
+
+
+                        //Console.WriteLine(Convert.ToString((byte)(((data[currentByte] & leftMask) << offset) + (data[currentByte + 1] & rightMask) >> (8 - offset)), 2).PadLeft(8, '0'));
+                        int t = result[result.Count - 3];
                         t <<= 8;
-                        t += result[result.Count - 1];
+                        t += result[result.Count - 2];
                         dict[t] = result[result.Count-1];
                     }
                     else
                     {
                         if (currentByte < data.Length)
                         {
-                            result.Add((byte)(((data[currentByte] & leftMask) << 8)));
-                            int t = result[result.Count - 2];
+                            //Console.WriteLine("written");
+                            //Console.WriteLine(Convert.ToString(leftMask, 2).PadLeft(8, '0'));
+                            //Console.WriteLine(Convert.ToString((byte)(((data[currentByte] & leftMask) << offset)), 2).PadLeft(8, '0'));
+
+                            result.Add((byte)(((data[currentByte] & leftMask) << offset)));
+                            int t = result[result.Count - 3];
                             t <<= 8;
-                            t += result[result.Count - 1];
+                            t += result[result.Count - 2];
                             dict[t] = result[result.Count-1];
+                            
                         }
                     }
 
@@ -263,7 +352,9 @@ namespace ClusterCompressor
 
 
             }
-            Console.WriteLine("undone");
+            //Console.WriteLine("undone");
+
+            //Console.WriteLine(result.Count());
             return result.ToArray();
         }
     }
